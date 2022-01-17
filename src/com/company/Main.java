177ae981784c8dea20ee8c1c04a8eddd30d8c433
Carmen Martín Núñez <carmen.martin@iesnervion.es>
2022-01-17 13:14:44 +0100

    commit 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cuadrado cuadrado = new Cuadrado(4);
+        Cuadrado cuadrado = new Cuadrado(3);
         cuadrado.toString();
 
         System.out.print(cuadrado);
